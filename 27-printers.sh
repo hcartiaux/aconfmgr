4b167af5f519f5ed9be1a71a512841d107c1bd17
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-05-19 10:53:10 +0200

    27-printers - office printer

--- 27-printers.sh
+++ 27-printers.sh
@@ -1,10 +1,10 @@
 # Printing
-IgnorePathsExcept /etc/cups/ printers.conf ppd/EPSON_ET-2820_Series.ppd
-
 AddPackage cups # OpenPrinting CUPS - daemon package
 AddPackage --foreign epson-inkjet-printer-escpr # Epson Inkjet Printer Driver (ESC/P-R) for Linux
 
-CreateLink /etc/cups/ppd/EPSON_ET-2820_Series.ppd /usr/share/ppd/epson-inkjet-printer-escpr/Epson-ET-2820_Series-epson-escpr-en.ppd
-CopyFile /etc/cups/printers.conf
+IgnorePath /etc/cups/
+#IgnorePathsExcept /etc/cups/ printers.conf ppd/EPSON_ET-2820_Series.ppd
+#CreateLink /etc/cups/ppd/EPSON_ET-2820_Series.ppd /usr/share/ppd/epson-inkjet-printer-escpr/Epson-ET-2820_Series-epson-escpr-en.ppd
+#CopyFile /etc/cups/printers.conf
 
 SystemdEnable cups /usr/lib/systemd/system/cups.service
