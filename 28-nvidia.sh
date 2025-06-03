c3670b9e23702c800cf372ae647eef15686a801a
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-06-03 11:30:43 +0200

    28-nvidia - package information

--- 28-nvidia.sh
+++ 28-nvidia.sh
@@ -1,11 +1,11 @@
 if lspci | grep -i "3D controller: NVIDIA" > /dev/null; then
 
     # Nvidia driver support
-    AddPackage nvidia
-    AddPackage nvidia-lts
-    AddPackage nvidia-prime
-    AddPackage nvidia-utils
-    AddPackage nvidia-prime-rtd3pm
+    AddPackage nvidiai # NVIDIA kernel modules
+    AddPackage nvidia-lts # NVIDIA drivers for linux-lts
+    AddPackage nvidia-prime # NVIDIA Prime Render Offload configuration and utilities
+    AddPackage nvidia-utils # NVIDIA drivers utilities
+    AddPackage --foreign nvidia-prime-rtd3pm # Configure your discrete NVIDIA GPU to power down when not in use.
 
     # OR power off the Nvidia chipset entirely
     # CopyFile /etc/udev/rules.d/80-nvidia-poweroff.rules
