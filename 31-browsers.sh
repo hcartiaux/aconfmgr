018f2ef62d80a69f0301e24d2d2086575914decd
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-06-06 08:39:40 +0200

    31-browsers - add firefox-pwa

--- 31-browsers.sh
+++ 31-browsers.sh
@@ -1,4 +1,5 @@
 AddPackage firefox # Fast, Private & Safe Web Browser
+AddPackage firefoxpwa # A tool to install, manage and use Progressive Web Apps (PWAs) in Mozilla Firefox (native component)
 AddPackage firefox-dark-reader # Inverts brightness of web pages and aims to reduce eyestrain while browsing the web
 AddPackage firefox-ublock-origin # Efficient blocker add-on for various browsers. Fast, potent, and lean
 AddPackage --foreign firefox-extension-plasma-integration # KDE plasma browser integration extension for firefox
