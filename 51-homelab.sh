5b0700efab13f34708bab28fa3aab351d15288f0
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-06-07 17:05:18 +0200

    51-homelab - reorg, + package lld

--- 51-homelab.sh
+++ 51-homelab.sh
@@ -1,3 +1,6 @@
+############################
+# Infrastructure management
+############################
 AddPackage opentofu # Lets you declaratively manage your cloud infrastructure
 AddPackage tflint # A linter for Terraform code
 AddPackage scaleway-cli # Command line interface for Scaleway Cloud
@@ -8,12 +11,27 @@ AddPackage cdrtools # Highly portable CD/DVD/BluRay command line recording softw
 AddPackage ansible # Official assortment of Ansible collections
 AddPackage ansible-lint # Checks playbooks for practices and behaviour that could potentially be improved.
 
-AddPackage act # Run your GitHub Actions locally
-
+###########
+# Homepage
+###########
 AddPackage hugo # Fast and Flexible Static Site Generator in Go
 
+############
+# Github CI
+############
+AddPackage act # Run your GitHub Actions locally
+
+###############
 # dn42 project
+###############
 AddPackage --foreign ca-certificates-dn42 # dn42 root certificates for *.dn42 and 172.22.0.0/15
 
+##############################################
 # OpenBSD cloud image build script dependency
+##############################################
 AddPackage signify # OpenBSD tool to cryptographically sign and verify files
+
+#########
+# 1kl-os
+#########
+AddPackage lld # Linker from the LLVM project
