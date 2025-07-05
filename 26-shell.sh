1fe13a03b5eaa892a2ed7c9bfd35a700920e9d64
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-07-05 19:55:32 +0200

    26-shell - add zsh-syntax-highlighting and powerline packages

--- 26-shell.sh
+++ 26-shell.sh
@@ -1,4 +1,10 @@
+AddPackage bash-completion # Programmable completion for the bash shell
+
 AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
 AddPackage zsh-completions # Additional completion definitions for Zsh
-AddPackage bash-completion # Programmable completion for the bash shell
+AddPackage zsh-syntax-highlighting # Fish shell like syntax highlighting for Zsh
+
+AddPackage powerline # Statuslines and prompts for several applications including vim, zsh, bash, tmux, IPython, Awesome, i3 and Qtile
+AddPackage powerline-fonts # Patched fonts for powerline
+
 AddPackage --foreign shellcheck-bin # Shell script analysis tool (binary release, static)
