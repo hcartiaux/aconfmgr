91213e78d74715317cb5ad6beec44eee2fb8a7e6
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-07-10 23:33:50 +0200

    03-helper-systemd - Fix SC2207 (Prefer mapfile or read -a to split command output (or quote to avoid splitting))

--- 03-helper-systemd.sh
+++ 03-helper-systemd.sh
@@ -93,7 +93,7 @@ function SystemdEnable() {
 
 		if grep -q WantedBy= "$unit_source"; then
 			IFS=$' \n\t'
-			wantedby=( $(grep -E '^WantedBy=' "$unit_source" | cut -d= -f2) )
+      mapfile -t wantedby < <(grep -E '^WantedBy=' "$unit_source" | cut -d= -f2)
 			IFS="$oIFS"
 			for target in "${wantedby[@]}"; do
 				CreateLink "/etc/systemd/${type}/${target}.wants/${name}" "${unit}"
@@ -108,7 +108,7 @@ function SystemdEnable() {
 
 		if grep -q Alias= "$unit_source"; then
 			IFS=$' \n\t'
-			aliases=( $(grep -E '^Alias=' "$unit_source" | cut -d= -f2) )
+      mapfile -t aliases < <(grep -E '^Alias=' "$unit_source" | cut -d= -f2)
 			IFS="$oIFS"
 			for target in "${aliases[@]}"; do
 				CreateLink "/etc/systemd/${type}/${target}" "${unit}"
