d2b78a5aa4381c7fe6f98977aa9d53e3c68584d2
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-07-24 16:50:31 +0200

    50-sysadmins - add ruby-term-ansicolor

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -44,6 +44,7 @@ AddPackage ruby-irb # Interactive Ruby command-line tool for REPL (Read Eval Pri
 AddPackage ruby-bundler # Manages an application's dependencies through its entire life, across many machines, systematically and repeatably
 AddPackage ruby-erb # An easy to use but powerful templating system for Ruby
 AddPackage ruby-rake # Make-like build tool implemented in Ruby
+AddPackage ruby-term-ansicolor # A ruby library that colors strings using ANSI escape sequences
 
 ## Python
 AddPackage python-pip # The PyPA recommended tool for installing Python packages
