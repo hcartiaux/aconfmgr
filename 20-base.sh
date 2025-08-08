206fe7aa218cd98fe684c0f92052dc2c04892d4a
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-08-08 17:44:15 +0200

    20-base,29-intel - Separated file for intel hardware support

--- 20-base.sh
+++ 20-base.sh
@@ -6,7 +6,6 @@ AddPackage linux-firmware # Firmware files for Linux
 AddPackage linux-headers # Headers and scripts for building modules for the Linux kernel
 AddPackage linux-lts # The LTS Linux kernel and modules
 AddPackage linux-lts-headers # Headers and scripts for building modules for the LTS Linux kernel
-AddPackage intel-ucode # Microcode update files for Intel CPUs
 AddPackage man-db # A utility for reading man pages
 
 CopyFile /etc/fstab
---
+++ 29-intel.sh
@@ -0,0 +1,5 @@
+AddPackage intel-ucode # Microcode update files for Intel CPUs
+AddPackage intel-gpu-tools # Tools for development and testing of the Intel DRM driver
+# AddPackage --foreign intel-npu-driver # Intel Neural Processing Unit (NPU) driver
+# AddPackage --foreign intel-media-driver # Intel Media Driver for VAAPI — Broadwell+ iGPUs
+# AddPackage --foreign libva-utils # Intel VA-API Media Applications and Scripts for libva
