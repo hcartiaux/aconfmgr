c6451bbae0d31b826a0659e960261cb604cd41c2
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-08-08 20:47:10 +0200

    29-intel - hw video decoding

--- 29-intel.sh
+++ 29-intel.sh
@@ -1,5 +1,6 @@
 AddPackage intel-ucode # Microcode update files for Intel CPUs
 AddPackage intel-gpu-tools # Tools for development and testing of the Intel DRM driver
+AddPackage vulkan-intel # Open-source Vulkan driver for Intel GPUs
+AddPackage intel-media-driver # Intel Media Driver for VAAPI — Broadwell+ iGPUs
+AddPackage libva-utils # Intel VA-API Media Applications and Scripts for libva
 # AddPackage --foreign intel-npu-driver # Intel Neural Processing Unit (NPU) driver
-# AddPackage --foreign intel-media-driver # Intel Media Driver for VAAPI — Broadwell+ iGPUs
-# AddPackage --foreign libva-utils # Intel VA-API Media Applications and Scripts for libva
