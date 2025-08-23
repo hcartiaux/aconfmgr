5562bf6f6fbc95ff2a8faf61cd8e320be358a00e
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-08-23 17:05:01 +0200

    20-nvidia - improved power management support, remove dependency on AUR package

--- 28-nvidia.sh
+++ 28-nvidia.sh
@@ -1,13 +1,20 @@
-if lspci | grep -i "3D controller: NVIDIA" > /dev/null; then
+pci_id=$(lspci | sed -n 's/\([^ ]*\) 3D controller: NVIDIA.*/\1/p' | xargs)
+
+if [ -n "${pci_id}" ] ; then
 
     # Nvidia driver support
     AddPackage nvidia # NVIDIA kernel modules
     AddPackage nvidia-lts # NVIDIA drivers for linux-lts
     AddPackage nvidia-prime # NVIDIA Prime Render Offload configuration and utilities
     AddPackage nvidia-utils # NVIDIA drivers utilities
-    AddPackage --foreign nvidia-prime-rtd3pm # Configure your discrete NVIDIA GPU to power down when not in use.
 
+    # Power management (Nvidia GPU D3Cold state support)
+    CopyFile /etc/modprobe.d/nvidia-pm.conf
+    CopyFile /etc/udev/rules.d/28-nvidia-pm.rules
     # OR power off the Nvidia chipset entirely
-    # CopyFile /etc/udev/rules.d/80-nvidia-poweroff.rules
+    # CopyFile /etc/udev/rules.d/28-nvidia-poweroff.rules
+
+    # TLP configuration (installed in 22-power-mgmt)
+    echo "RUNTIME_PM_ENABLE=\"${pci_id}\"" > "$(CreateFile /etc/tlp.d/28-nvidia.conf)"
 
 fi
