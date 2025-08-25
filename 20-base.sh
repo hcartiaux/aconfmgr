053111518add948561bc17e9f1d4e23c382bace8
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-08-25 17:40:46 +0200

    20-base - enable paccache systemd timer from pacman-contrib

--- 20-base.sh
+++ 20-base.sh
@@ -75,10 +75,12 @@ CopyFile /etc/makepkg.conf
 CopyFile /etc/pacman.conf
 AddPackage devtools # Tools for Arch Linux package maintainers
 AddPackage arch-signoff # Sign off Arch Linux testing packages
-AddPackage pacman-contrib # Contributed scripts and tools for pacman systems
 AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+
+AddPackage pacman-contrib # Contributed scripts and tools for pacman systems
+SystemdEnable pacman-contrib /usr/lib/systemd/system/paccache.timer
 
 # Security
 CopyFile /etc/sudoers
