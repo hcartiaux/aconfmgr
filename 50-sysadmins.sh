15318afd5767b25897194ccb72f553ea5fc0025e
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-10-09 10:26:11 +0200

    50-sysadmins - install java 8, necessary for old BMCs console redirection...

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -64,6 +64,7 @@ AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to t
 
 ## Java
 AddPackage jdk-openjdk # OpenJDK Java 24 full runtime environment
+AddPackage jre8-openjdk # OpenJDK Java 8 full runtime environment
 AddPackage icedtea-web # Additional components for OpenJDK - Browser plug-in and Web Start implementation
 
 ## Node
