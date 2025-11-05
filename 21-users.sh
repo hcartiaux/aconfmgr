c6b804051087c2e282a204733209d4d6891d67ce
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-11-05 11:33:07 +0100

    21-users - add myself to group lp for easy printing

--- 21-users.sh
+++ 21-users.sh
@@ -1,5 +1,6 @@
 for user in hcartiaux hcartiaux.perso; do
     #AddUser "${user}" "$(GetPassword $user)" 1000 1000 x 'Hyacinthe Cartiaux' "/home/${user}" /usr/bin/zsh ''
     AddUserToGroup "${user}" wheel
+    AddUserToGroup "${user}" lp
     CreateFile "/var/lib/systemd/linger/${user}" > /dev/null
 done
