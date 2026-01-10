f507a848c8faa63142e444e443f1a5bf87c33ba2
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-01-10 01:16:35 +0100

    Reapply "Remove some AUR packages"
    This reverts commit b82b83d134fecd9ce4045aafa64df535767d82b7.

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -61,7 +61,6 @@ AddPackage --foreign python-gitchangelog # Creates a changelog from git log hist
 ## Puppet
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
 AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
-AddPackage --foreign python-pypuppetdb # Library for working with the PuppetDB REST API
 AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to the style guide.
 
 ## Java
