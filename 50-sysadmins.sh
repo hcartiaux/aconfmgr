780652a3d57079c6efab611ca43609b2baa9a0b5
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-01-16 10:02:58 +0100

    50-sysadmins - +asciinema

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -39,6 +39,10 @@ AddPackage podman # Tool and library for running OCI-based containers in pods
 AddPackage podman-compose # A script to run docker-compose.yml using podman
 CopyFile /etc/containers/registries.conf.d/10-unqualified-search-registries.conf
 
+# Terminal screencast recording
+AddPackage asciinema # Record and share terminal sessions
+AddPackage --foreign asciinema-agg # asciinema gif generator
+
 # Languages
 
 ## Ruby
