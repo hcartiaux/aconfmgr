3bae520040bf9801d69a04c73282342ae1ec6318
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-02-26 15:42:25 +0100

    50-sysadmins - add python-jq (used with nvchecker)

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -64,6 +64,7 @@ AddPackage python-pip # The PyPA recommended tool for installing Python packages
 AddPackage python-virtualenv # Virtual Python Environment builder
 AddPackage python-rich # Render rich text, tables, progress bars, syntax highlighting, markdown and more to the terminal
 AddPackage python-dnspython # A DNS toolkit for Python
+AddPackage python-jq # Python bindings for jq
 AddPackage python-ldap # LDAP client API for Python
 AddPackage python-xmljson # Converts XML into JSON/Python dicts/arrays and vice-versa.
 AddPackage python-flake8 # The modular source code checker: pep8, pyflakes and co
