9cfebc845e128189581cee0090368009ac01f8d5
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-03-13 11:02:14 +0100

    50-sysadmins - configure autofs and cvmfs

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -44,10 +44,15 @@ AddPackage asciinema # Record and share terminal sessions
 AddPackage --foreign asciinema-agg # asciinema gif generator
 
 # CernVM-FS and EESSI
+AddPackage --foreign autofs # A kernel-based automounter for Linux
 AddPackage --foreign cvmfs # A client-server file system implemented in FUSE and developed to deliver software distributions onto virtual machines in a fast, scalable, and reliable way.
 AddPackage --foreign cvmfs-config-eessi # CernVM-FS configuration for the EESSI project
 AddPackage --foreign lmod # Environment modules system based on Lua that handles MODULEPATH hierarchical problem. Supports also legacy TCL modules
-CreateDir /cvmfs/software.eessi.io '' cvmfs cvmfs
+
+CopyFile /etc/autofs/auto.master.d/cvmfs.autofs
+IgnorePath /cvmfs
+IgnorePath /misc
+IgnorePath /net
 
 # Languages
 
