9c0db6d012ccf31613d2e7d60fc0b012e1edfd25
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-04-21 01:38:02 +0200

    50-sysadmins - install tree-sitter-*

--- 50-sysadmins.sh
+++ 50-sysadmins.sh
@@ -115,6 +115,8 @@ AddPackage --foreign vim-puppet # Puppet language syntax highlighting for vim.
 # Neovim and its dependencies
 AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
 AddPackage python-pynvim # Python client for Neovim
+AddPackage tree-sitter-cli # CLI tool for developing, testing, and using Tree-sitter parsers
+AddPackageGroup tree-sitter-grammars
 AddPackage lua51 # Powerful lightweight programming language designed for extending applications
 AddPackage luarocks # Deployment and management system for Lua modules
 AddPackage ast-grep # CLI tool for code structural search, lint and rewriting
