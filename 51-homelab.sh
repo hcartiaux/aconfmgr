3acf5b1f2db3ae3fd195f03e3e8c5fb4506f34cb
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-04-20 22:47:57 +0200

    51-homelab - add ansible-language-server

--- 51-homelab.sh
+++ 51-homelab.sh
@@ -10,6 +10,7 @@ AddPackage cdrtools # Highly portable CD/DVD/BluRay command line recording softw
 
 AddPackage ansible # Official assortment of Ansible collections
 AddPackage ansible-lint # Checks playbooks for practices and behaviour that could potentially be improved.
+AddPackage ansible-language-server # Ansible Language Server
 
 ###########
 # Homepage
