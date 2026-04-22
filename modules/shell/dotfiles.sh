587c6bf1c9916484c46bf0c11ceb1f06e0b9c5d9
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-04-22 14:54:55 +0200

    [shell] new flavour dotfiles

---
+++ modules/shell/dotfiles.sh
@@ -0,0 +1,18 @@
+# Dotfiles
+
+if [ "$aconfmgr_action" = "apply" ]; then
+    DOTFILES_DIR="$HOME/.dotfiles"
+    DOTFILES_REPO="https://codeberg.org/hcartiaux/dotfiles.git"
+
+    LogEnter 'Applying dotfiles...\n'
+    if [ ! -d "$DOTFILES_DIR" ]; then
+       git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
+    fi
+
+    {
+        cd "$DOTFILES_DIR" || FatalError "Dotfiles directory missing"
+        git pull && ./install.sh
+    }
+
+    LogLeave
+fi
--- profiles/hc-xps15.sh
+++ profiles/hc-xps15.sh
@@ -1,6 +1,6 @@
 ModuleLoad hardware   intel nvidia printers power-mgmt sound bluetooth fw-mgmt
 ModuleLoad desktop    plasma web mail apps
-ModuleLoad shell      bash zsh editors utils
+ModuleLoad shell      bash zsh dotfiles editors utils
 ModuleLoad dev        archlinux shell python ruby java node
 ModuleLoad ops        ssh ansible puppet terraform
 ModuleLoad virt       podman qemu vbox
