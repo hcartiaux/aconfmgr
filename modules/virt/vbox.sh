21e98862a2f6a199dc50d0f53091af1a33d318b4
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-04-24 21:42:59 +0200

    [modules] virt/vbox - replace the dkms module to speed-up kernel updates

--- modules/virt/vbox.sh
+++ modules/virt/vbox.sh
@@ -1,5 +1,6 @@
 # VirtualBox
 AddPackage virtualbox # Powerful x86 virtualization for enterprise as well as home use
-AddPackage virtualbox-host-dkms # VirtualBox Host kernel modules sources
+AddPackage virtualbox-host-modules-arch # Virtualbox host kernel modules for Arch Kernel
+AddPackage virtualbox-host-modules-lts # Virtualbox host kernel modules for LTS Kernel
 AddPackage virtualbox-guest-iso # The official VirtualBox Guest Additions ISO image
 AddPackage --foreign vagrant # Build and distribute virtualized development environments
