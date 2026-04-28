ec5d1669f948ae77d3b6e82cc848c30151575864
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-04-28 14:15:49 +0200

    [modules] ops/ansible - remove language server (orphaned)

--- modules/ops/ansible.sh
+++ modules/ops/ansible.sh
@@ -1,4 +1,3 @@
 # Ansible
 AddPackage ansible # Official assortment of Ansible collections
 AddPackage ansible-lint # Checks playbooks for practices and behaviour that could potentially be improved.
-AddPackage ansible-language-server # Ansible Language Server
