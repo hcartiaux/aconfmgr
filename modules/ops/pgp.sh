9c16552049ed92378fc25c5ee3306e276fc177bd
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-06-10 00:16:18 +0200

    [profiles,modules] new file ops/pgp

---
+++ modules/ops/pgp.sh
@@ -0,0 +1,5 @@
+# GnuPG + nitrokey set-up
+AddPackage openpgp-card-tools # CLI tool to inspect, manage and use OpenPGP cards
+AddPackage sequoia-sq # Command-line frontends for Sequoia
+AddPackage nitrokey-udev-rules # Udev rules for Nitrokey devices
+AddPackage python-pynitrokey # A command line interface for the Nitrokey FIDO2, Nitrokey Start, Nitrokey 3 and NetHSM
--- profiles/hc-xps15.sh
+++ profiles/hc-xps15.sh
@@ -2,6 +2,6 @@ ModuleLoad hardware   intel nvidia printers power-mgmt sound bluetooth fw-mgmt
 ModuleLoad desktop    plasma web mail apps
 ModuleLoad shell      bash zsh dotfiles utils
 ModuleLoad dev        editors archlinux shell python ruby java node
-ModuleLoad ops        ssh ansible puppet terraform
+ModuleLoad ops        pgp ssh ansible puppet terraform
 ModuleLoad virt       podman qemu vbox
 ModuleLoad work       mkdocs eessi
