73bfb3066cfb18e9ec7a3c345056b0a30151b8c6
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-06-15 14:15:53 +0200

    [modules] dev/shell - install shellcheck from extra

--- modules/dev/shell.sh
+++ modules/dev/shell.sh
@@ -3,5 +3,5 @@ AddPackage bats # Bash Automated Testing System
 AddPackage bats-assert # Common assertions for Bats
 AddPackage bats-file # Common filesystem assertions for Bats
 
-AddPackage --foreign shellcheck-bin # Shell script analysis tool (binary release, static)
+AddPackage shellcheck # Shell script analysis tool
 AddPackage shfmt # Format shell programs
