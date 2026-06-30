ded84744c2aa8545a2938f1ad6565f7805120b7a
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-06-30 16:43:24 +0200

    [modules] hardware/printer - reorg and system-config-printer

--- modules/hardware/printers.sh
+++ modules/hardware/printers.sh
@@ -1,11 +1,11 @@
 # Printing
 AddPackage cups # OpenPrinting CUPS - daemon package
 AddPackage --foreign epson-inkjet-printer-escpr # Epson Inkjet Printer Driver (ESC/P-R) for Linux
+AddPackage system-config-printer # A CUPS printer configuration tool and status applet
 
-IgnorePathsExcept /etc/cups ppd/EPSON_ET-2820_Series.ppd # printers.conf
-CreateLink /etc/cups/ppd/EPSON_ET-2820_Series.ppd /usr/share/ppd/epson-inkjet-printer-escpr/Epson-ET-2820_Series-epson-escpr-en.ppd
 # CopyFile /etc/cups/printers.conf
-
+IgnorePathsExcept /etc/cups ppd/EPSON_ET-2820_Series.ppd
+CreateLink /etc/cups/ppd/EPSON_ET-2820_Series.ppd /usr/share/ppd/epson-inkjet-printer-escpr/Epson-ET-2820_Series-epson-escpr-en.ppd
 CreateFile /etc/samba/smb.conf > /dev/null
 
 SystemdEnable cups /usr/lib/systemd/system/cups.service
