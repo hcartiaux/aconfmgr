13eb97141be1317ad5c725e991b98a283564999a
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-07-17 23:12:25 +0200

    [modules] ops/ssh - clustershell enters extra-testing

--- modules/ops/ssh.sh
+++ modules/ops/ssh.sh
@@ -2,7 +2,7 @@
 AddPackage openssh # SSH protocol implementation for remote login, command execution and file transfer
 AddPackage sshuttle # Transparent proxy server that forwards all TCP packets over ssh
 AddPackage sshpass # Fool ssh into accepting an interactive password non-interactively
-AddPackage --foreign clustershell # Python framework for efficient cluster administration
+AddPackage clustershell # Python framework for efficient cluster administration
 
 ## Enable ssh-agent
 CopyFile "/etc/systemd/user/ssh-agent.service"
