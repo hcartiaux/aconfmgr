4fd50417437b18164cb912dfcd35b58518355150
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-07-21 21:17:18 +0200

    Revert "[modules] virt/vbox - replace the dkms module to speed-up kernel updates"
    This reverts commit 21e98862a2f6a199dc50d0f53091af1a33d318b4.

--- modules/virt/vbox.sh
+++ modules/virt/vbox.sh
@@ -1,6 +1,5 @@
 # VirtualBox
 AddPackage virtualbox # Powerful x86 virtualization for enterprise as well as home use
-AddPackage virtualbox-host-modules-arch # Virtualbox host kernel modules for Arch Kernel
-AddPackage virtualbox-host-modules-lts # Virtualbox host kernel modules for LTS Kernel
+AddPackage virtualbox-host-dkms # VirtualBox Host kernel modules sources
 AddPackage virtualbox-guest-iso # The official VirtualBox Guest Additions ISO image
 AddPackage --foreign vagrant # Build and distribute virtualized development environments
