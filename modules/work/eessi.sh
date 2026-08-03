27c64442b163cdb9d59e127970deb4a296a0fa7a
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-08-03 19:15:02 +0200

    [modules] work/eessi - lmod moved to extra

--- modules/work/eessi.sh
+++ modules/work/eessi.sh
@@ -2,7 +2,7 @@
 AddPackage --foreign autofs # A kernel-based automounter for Linux
 AddPackage --foreign cvmfs # A client-server file system implemented in FUSE and developed to deliver software distributions onto virtual machines in a fast, scalable, and reliable way.
 AddPackage --foreign cvmfs-config-eessi # CernVM-FS configuration for the EESSI project
-AddPackage --foreign lmod # Environment modules system based on Lua that handles MODULEPATH hierarchical problem. Supports also legacy TCL modules
+AddPackage lmod # Environment modules system based on Lua that handles MODULEPATH hierarchical problem. Supports also legacy TCL modules
 
 CopyFile /etc/autofs/auto.master.d/cvmfs.autofs
 IgnorePath /cvmfs
