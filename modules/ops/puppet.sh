12fc44cb340a5b2276beccd317d561f89c9562fe
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-08-04 23:20:34 +0200

    [modules] ops/puppet - ruby-hiera-eyaml moved to extra

--- modules/ops/puppet.sh
+++ modules/ops/puppet.sh
@@ -1,5 +1,5 @@
 # Puppet
 AddPackage r10k # Provides a general purpose toolset for deploying Puppet environments and modules.
-AddPackage --foreign ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
+AddPackage ruby-hiera-eyaml # OpenSSL Encryption backend for Hiera
 AddPackage --foreign puppet-lint # Check that your Puppet manifests conform to the style guide.
 AddPackage --foreign vim-puppet # Puppet language syntax highlighting for vim.
